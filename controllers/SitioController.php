1c7ad4923399151776c9bdde84c3c13b5dfc2c94
dounorquix <92748835+dounorquix@users.noreply.github.com>
2024-05-30 11:25:16 -0400

    Update SitioController.php
    create controller and view

--- controllers/SitioController.php
+++ controllers/SitioController.php
@@ -1,1 +1,22 @@
-<?php?>
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessController;
+use yii\web\Controller;
+
+class SitioController extends Controller{
+
+   public function actionInicio(){
+
+       return $this->render('inicio');
+   }
+  
+}
+
+
+
+
+
+?>
